d9bf7dde6a076e92a6377b2c739a7ea5ac328f7f
Nathnael Yirga <natty@localhost.localdomain>
2024-01-05 21:26:04 +0300

    code improved

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from libgen_api import LibgenSearch
 import requests
 import urllib.request
 
-
 def inp_to_int(inp):
 	try:
 		i = int(inp)
@@ -15,7 +14,7 @@ def inp_to_int(inp):
 
 s = LibgenSearch()
 
-search_choice = input("Enter 1 to search by author,any number for search by title.")
+search_choice = inp_to_int(input("Enter 1 for search by author,any number for search by title."))
 
 if search_choice == 1:
 	author = input("Who is the author?")
@@ -28,45 +27,30 @@ else:
 for result in results:
 	author = result["Author"]
 	title = result["Title"]
-	publisher = result["Publisher"]
-	title = result["Title"]
 	size = result["Size"]
 	extention = result["Extension"]
-	mirrors = [result["Mirror_1"], result["Mirror_2"]]
+	# publisher = result["Publisher"]
+	# mirrors = [result["Mirror_1"], result["Mirror_2"]]
 
 	print(f"[*]{results.index(result)}.{title}\nBy:{author}\nSize:{size}({extention}\n)")
 
-book = inp_to_int(input("Select the book for download."))
 
-book = results[book]
+book = results[inp_to_int(input("Select the book for download."))]
 
 response = requests.get(book["Mirror_1"])
-# response = requests.get("http://library.lol/main/BE9884DFAD35CF3C240B774E8810413B")
 
-#BeautifulSoup
+#BeautifulSoup gets the download link
 soup = BeautifulSoup(response.content, 'html.parser') 
 
 div = soup.find('div', id="download")
-book_link = div.h2.a.get('href')
-# print(div.h2.a.get('href'))
-print(book_link)
+download_link = div.h2.a.get('href')
 
 file_name = book["Title"]+"."+book["Extension"]
 
-f = open(file_name, "w")
-f.close()
-
-print("Requisting book, please wait.....")
-# book_request = requests.get(book_link)
-urllib.request.urlretrieve(book_link, "Downloads/"+file_name)
-
-# print("Book recieved, saving ....")
-# # print(response)
-# with open(book["Title"]+"."+book["Extension"], "wb") as f:
-# 	f.write(response.content)
-
-# print(results[0]["Mirror_1"])
+print("[DOWNLOADING] Requisting book, please wait.....")
+urllib.request.urlretrieve(download_link, "./Downloads/"+file_name)
 
 
 
-print(f"{book['Title']}Saved")
+
+print(f"[DONE] {book['Title']} Saved!")
